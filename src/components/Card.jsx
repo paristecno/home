5358813fa523f289e0f9e67bbe93c5cba181da42
paristecno <paristecnoargentina@gmail.com>
2024-05-21 19:02:23 -0300

    asd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Card = () => {
-  return (
-	<div className='text-6xl text-red-500 font-mono flex justify-center mt-72'>Card</div>
-  )
-}
+	const [mostrarDetalle, setmostrarDetalle] = useState(false);
+	const [mostrarCheckout, setmostrarCheckout] = useState(false);
+  
+	return (
+	  <div className="flex justify-center items-center h-screen">
+		{!mostrarDetalle && !mostrarCheckout && (
+		  <button onClick={() => setmostrarDetalle(true)} className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
+			Landing
+		  </button>
+		)}
+		{mostrarDetalle && !mostrarCheckout && (
+		  <button onClick={() => setmostrarCheckout(true)} className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">
+			Detail
+		  </button>
+		)}
+		{mostrarCheckout && (
+		  <button className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">
+			Checkout
+		  </button>
+		)}
+	  </div>
+	);
+  };
+  
 
 export default Card
