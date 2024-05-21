ad4acd47cc20b614fb1e8d89456a672b48b0f254
paristecno <paristecnoargentina@gmail.com>
2024-05-21 18:32:21 -0300

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   // otras configuraciones de Astro
-  outDir: 'dist', // Este es el directorio de salida por defecto
+  outDir: 'dist' // Este es el directorio de salida por defecto
+  ,
+
+  integrations: [react(), tailwind()]
 });
