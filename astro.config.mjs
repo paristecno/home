1fa57c2d7b9e3e2d88bcab7903cfe78dba2845e9
paristecno <paristecnoargentina@gmail.com>
2024-05-21 18:48:07 -0300

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // otras configuraciones de Astro
-  outDir: 'dist' // Este es el directorio de salida por defecto
-  ,
+  // Añade la base URL de tu proyecto
+  base: '/home', // Reemplaza 'home' con el nombre de tu repositorio
 
+  // Otras configuraciones de Astro
+  outDir: 'dist', // Este es el directorio de salida por defecto
   integrations: [react(), tailwind()]
 });
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = () => {
   return (
-	<div className='text-4xl text-red-500 font-mono'>Card</div>
+	<div className='text-6xl text-red-500 font-mono flex justify-center mt-72'>Card</div>
   )
 }
 
