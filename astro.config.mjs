9276bb05467aa25307be0bcbb5510caebe5213a2
paristecno <paristecnoargentina@gmail.com>
2024-05-21 18:29:49 -0300

    aver

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // otras configuraciones de Astro
+  outDir: 'dist', // Este es el directorio de salida por defecto
+});
