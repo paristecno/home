e55bc80ea8f1fe4ac8f30732539afa623ccf20f9
paristecno <paristecnoargentina@gmail.com>
2024-05-26 19:13:54 -0300

    a

--- src/components/Mp/Erro.jsx
+++ src/components/Mp/Erro.jsx
@@ -25,10 +25,10 @@ const Erro = () => {
        <div className="h-screen flex flex-col">
        {/* Parte superior (roja) */}
        <div className="bg-red-500 flex justify-center items-center h-1/2">
-        <div className='absolute top-4 font-bold text-4xl text-gray-300  ml-[400px]'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+       <a href="/home" className='absolute top-4 font-bold text-4xl text-gray-300  ml-[400px]'> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
 </svg>
-</div>
+</a>
          <div className="text-center">
            {/* Imagen centrada */}
            <img src="https://i.postimg.cc/rmWtw51t/removal-ai-c1562fe4-f428-4b5d-a15c-7585e1e795c2-screen.png" alt="Imagen" className="w-32 h-32 mx-auto mb-4" />
