7b64df3c74d67289bbdbb1136766781e3ef97823
paristecno <paristecnoargentina@gmail.com>
2024-05-26 20:49:26 -0300

    mmm

--- src/components/Mp/Mp.jsx
+++ src/components/Mp/Mp.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState,useRef} from 'react';
 import Navas from './Navas';
 import Carga from './Carga';
 import Erro from './Erro';
